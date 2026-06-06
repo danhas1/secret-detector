fcb49fee528a0f09fbd7c671b4fe3e3efabf32d7
DAb <danhason@MBP-sl-dn.bbrouter>
2026-06-06 18:14:51 +0300

    fix cors production

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="SecretScanner API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],
+    allow_origins=[״*״],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
